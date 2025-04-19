3463b0a39ba93bf9ad8e685dcd74401ddfc0a885
Aviral Shukla <130632823+getchaviral@users.noreply.github.com>
2025-04-19 18:56:55 +0530

    Update remove_dubli.py

--- remove_dubli.py
+++ remove_dubli.py
@@ -1,17 +1,8 @@
-def removeDupli(arr):
-    n = len(arr)
-    if n <= 1:
-        return n
-    idx = 1  
-    
-    for i in range(1, n):
-        if arr[i] != arr[i - 1]:
-            arr[idx] = arr[i]
-            idx += 1
+def remove_duplis(arr):
+    x1= list(set(arr))
+    return x1
 
-    return idx
-arr = [2, 2, 3, 4, 4, 5, 5, 6, 7, 7]  # Your input array
-arr_without_dupli = removeDupli(arr)
 
-# Output the result
-print("Array without dupli:", arr_without_dupli)
+
+arr = [1, 2, 2, 3, 4, 4]
+print(remove_duplis(arr))  
