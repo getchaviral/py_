527ede7a3dc52c1ebdf11bfa4346e6c238a3a307
Aviral Shukla <130632823+getchaviral@users.noreply.github.com>
2025-04-19 18:49:26 +0530

    Update remove_sort_dub.py

--- remove_sort_dub.py
+++ remove_sort_dub.py
@@ -1,16 +1,2 @@
-def removeDuplicates(arr):
-     arr = [1, 2, 2, 3, 4, 5, 5]
-
-    
-     if not arr:
-        return []
-
-     result = [arr[0]]  
-
-     for i in range(1, len(arr)):
-        if arr[i] != arr[i - 1]:  
-            result.append(arr[i])
-
-     return result
 
 
