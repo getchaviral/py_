23367b56aae2d4c0a3869976a6fa1b810e8f57c7
Aviral Shukla <130632823+getchaviral@users.noreply.github.com>
2025-04-19 18:09:20 +0530

    Update leaders.py

--- leaders.py
+++ leaders.py
@@ -1,13 +1,16 @@
-arr = [1,2,3,4,5,5,6,6,7,8]
-n = len(arr)
-result = []
-
-for i in range(n):
-    for j in range(i + 1, n):
-        if arr[i] < arr[j]:
-            break
-    else:
-        result.append(arr[i])
-
-print("Leaders in array:")
-print(" ".join(map(str, result)))
+def leaders(arr):        
+        n= len(arr)
+        res=[]
+        
+        for i in range(n):
+            
+            for j in range(i+1,n):
+                if arr[i] < arr[j]:
+                  break
+            else:   
+                res.append(arr[i])
+                    
+        return res
+arr=[1,2,3,4,5,4,3,2,2]
+n= len(arr)
+print(leaders(arr))  
